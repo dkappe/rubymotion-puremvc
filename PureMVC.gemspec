f955327891153308334974da067036a251691c68
Dietrich Kappe <dkappe@pathf.com>
2013-05-13 12:14:08 -0500

    Initial checkin.

---
+++ PureMVC.gemspec
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+VERSION = "1.0"
+
+Gem::Specification.new do |spec|
+  spec.name          = "PureMVC"
+  spec.version       = VERSION
+  spec.authors       = ["Dietrich Kappe"]
+  spec.email         = ["dkappe@pathf.com"]
+  spec.description   = %q{Gem for Ruby PureMVC}
+  spec.summary       = %q{Has special singleton that uses GCD.}
+  spec.homepage      = ""
+  spec.license       = ""
+
+  files = []
+  files << 'README.md'
+  files.concat(Dir.glob('lib/**/*.rb'))
+  spec.files         = files
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "rake"
+end
---
+++ lib/project/patterns/puremvc_singleton.rb
@@ -0,0 +1,188 @@
+# The Singleton module implements the Singleton pattern.
+#
+# == Usage
+#
+# To use Singleton, include the module in your class.
+#
+#    class Klass
+#       include Singleton
+#       # ...
+#    end
+#
+# This ensures that only one instance of Klass can be created.
+#
+#      a,b  = Klass.instance, Klass.instance
+#
+#      a == b
+#      # => true
+#
+#      Klass.new
+#      # => NoMethodError - new is private ...
+#
+# The instance is created at upon the first call of Klass.instance().
+#
+#      class OtherKlass
+#        include Singleton
+#        # ...
+#      end
+#
+#      ObjectSpace.each_object(OtherKlass){}
+#      # => 0
+#
+#      OtherKlass.instance
+#      ObjectSpace.each_object(OtherKlass){}
+#      # => 1
+#
+#
+# This behavior is preserved under inheritance and cloning.
+#
+# == Implementation
+#
+# This above is achieved by:
+#
+# *  Making Klass.new and Klass.allocate private.
+#
+# *  Overriding Klass.inherited(sub_klass) and Klass.clone() to ensure that the
+#    Singleton properties are kept when inherited and cloned.
+#
+# *  Providing the Klass.instance() method that returns the same object each
+#    time it is called.
+#
+# *  Overriding Klass._load(str) to call Klass.instance().
+#
+# *  Overriding Klass#clone and Klass#dup to raise TypeErrors to prevent
+#    cloning or duping.
+#
+# == Singleton and Marshal
+#
+# By default Singleton's #_dump(depth) returns the empty string. Marshalling by
+# default will strip state information, e.g. instance variables and taint
+# state, from the instance. Classes using Singleton can provide custom
+# _load(str) and _dump(depth) methods to retain some of the previous state of
+# the instance.
+#
+#    require 'singleton'
+#
+#    class Example
+#      include Singleton
+#      attr_accessor :keep, :strip
+#      def _dump(depth)
+#        # this strips the @strip information from the instance
+#        Marshal.dump(@keep, depth)
+#      end
+#
+#      def self._load(str)
+#        instance.keep = Marshal.load(str)
+#        instance
+#      end
+#    end
+#
+#    a = Example.instance
+#    a.keep = "keep this"
+#    a.strip = "get rid of this"
+#    a.taint
+#
+#    stored_state = Marshal.dump(a)
+#
+#    a.keep = nil
+#    a.strip = nil
+#    b = Marshal.load(stored_state)
+#    p a == b  #  => true
+#    p a.keep  #  => "keep this"
+#    p a.strip #  => nil
+#
+module PureMVCSingleton
+  # Raises a TypeError to prevent cloning.
+  def clone
+    raise TypeError, "can't clone instance of singleton #{self.class}"
+  end
+
+  # Raises a TypeError to prevent duping.
+  def dup
+    raise TypeError, "can't dup instance of singleton #{self.class}"
+  end
+
+  # By default, do not retain any state when marshalling.
+  def _dump(depth = -1)
+    ''
+  end
+
+  module SingletonClassMethods # :nodoc:
+
+    def clone # :nodoc:
+      PureMVCSingleton.__init__(super)
+    end
+
+    # By default calls instance(). Override to retain singleton state.
+    def _load(str)
+      instance
+    end
+
+    private
+
+    def inherited(sub_klass)
+      super
+      PureMVCSingleton.__init__(sub_klass)
+    end
+  end
+
+  class << PureMVCSingleton # :nodoc:
+    def __init__(klass) # :nodoc:
+      klass.instance_eval {
+        @singleton__instance__ = nil
+      }
+      def klass.instance # :nodoc:
+        return @singleton__instance__ if @singleton__instance__
+
+        queue = SingletonQueue.queue(self.name)
+
+        queue.sync {
+          return @singleton__instance__ if @singleton__instance__
+          @singleton__instance__ = new()
+        }
+        @singleton__instance__
+      end
+      klass
+    end
+
+    private
+
+    def append_features(mod)
+      #  help out people counting on transitive mixins
+      unless mod.instance_of?(Class)
+        raise TypeError, "Inclusion of the OO-Singleton module in module #{mod}"
+      end
+      super
+    end
+
+    def included(klass)
+      super
+      klass.private_class_method :new, :allocate
+      klass.extend SingletonClassMethods
+      Singleton.__init__(klass)
+    end
+  end
+
+  ##
+  # :singleton-method: _load
+  #  By default calls instance(). Override to retain singleton state.
+  class SingletonQueue
+    @@myq = nil
+    @@queue = nil
+
+    def self.queue(qname)
+      Dispatch.once do
+        @@myq = Dispatch::Queue.new('org.puremvc.__singleton__')
+        @@queue = {}
+      end
+      @@myq.sync do
+        if @@queue[qname].nil?
+          @@queue[qname] = Dispatch::Queue.new("org.puremvc.__singleton__.#{qname}")
+          #puts "Created queue for #{qname}"
+        end
+      end
+      @@queue[qname]
+    end
+  end
+end
+
